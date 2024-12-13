79db26f00c0e1ae08af834c0ccd0c46c91a40451
Yini Duanmu <yini@Yinis-MacBook-Pro.local>
2024-12-13 00:41:37 -0800

    cookie

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,11 @@
 import express from "express";
 import routers from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser("hello-world-secret-key"));
 app.use(routers);
 
 const PORT = process.env.PORT || 3000;
@@ -27,6 +29,7 @@ app.get(
     next();
   },
   (request, response) => {
+    response.cookie("hello", "world", { maxAge: 60000 * 60 * 2, signed: true });
     response.status(201).send({ msg: "Hello!" });
   }
 );
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,7 +3,14 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (request, response) => {
-  response.send([{ id: 1, name: "chicken breast", price: 10.99 }]);
+  console.log(request.headers.cookie);
+  console.log(request.cookies);
+  console.log(request.signedCookies);
+  console.log(request.signedCookies.hello);
+  if (request.signedCookies.hello && request.signedCookies.hello === "world") {
+    return response.send([{ id: 1, name: "chicken breast", price: 10.99 }]);
+  }
+  return response.send("Sorry, you need the correct cookies");
 });
 
 export default router;
